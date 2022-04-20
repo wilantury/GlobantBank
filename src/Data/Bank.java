5aeef8efc76d2852576d93eb90edfd26ab512a2a
Wilson Fernando Antury Torres <wilson.antury@globant.com>
2022-04-20 08:15:11 -0500

    [ADD] Bank model

--- src/Data/Bank.java
+++ src/Data/Bank.java
@@ -1,4 +1,25 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+    private ArrayList<User> users;
+
+    public Bank(String name){
+        this.name = name;
+        users = new ArrayList<>();
+    }
+
+    public void addUser(User user){
+        this.users.add(user);
+    }
+
+    public void removeUser(User user){
+        this.users.remove(user);
+    }
+
+    public ArrayList<User> getUsers() {
+        return this.users;
+    }
 }
