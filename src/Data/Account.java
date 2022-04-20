8a4fb16f27aa3c7166471482855284d14b60fb47
Wilson Fernando Antury Torres <wilson.antury@globant.com>
2022-04-20 09:01:03 -0500

    [ADD] Account model

--- src/Data/Account.java
+++ src/Data/Account.java
@@ -1,4 +1,43 @@
 package Data;
 
+import org.omg.CORBA.Current;
+
+import java.time.Instant;
+
+
 public class Account {
+    private String accountNumber;
+    private float balance = 0;
+    private Instant openDate;
+
+    public Account(String accountNumber, float balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.openDate = Instant.now();
+    }
+
+    //Getters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public Instant getOpenDate() {
+        return openDate;
+    }
+
+    public void withdraw(float amount){
+
+    }
+
+    public void addMoney(float amount){
+
+    }
+
+    public void transferMoney(float amount, SavingAccount originAccount, SavingAccount targetAccount){
+
+    }
 }
--- src/Data/SavingAccount.java
+++ src/Data/SavingAccount.java
@@ -1,4 +1,26 @@
 package Data;
 
-public class SavingAccount {
+public class SavingAccount extends Account{
+    private float withdrawTaxFix = 200;
+    private float withdrawTaxPercentage = 0.15f;
+    private float transferTaxFix = 100;
+
+    public SavingAccount(String name, float balance){
+        super(name,balance);
+    }
+
+    public SavingAccount(String name, float balance, float withdrawTaxFix, float withdrawTaxPercentage, float transferTaxFix){
+        super(name,balance);
+        this.transferTaxFix = transferTaxFix;
+        this.withdrawTaxFix = withdrawTaxFix;
+        this.withdrawTaxPercentage = withdrawTaxPercentage;
+    }
+
+    private float calcTaxWithdraw(float amount){
+        return 0;
+    }
+
+    private float calcTaxTransfer(float amount){
+        return 0;
+    }
 }
